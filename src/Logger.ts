31714c0debaf74aa1843aa8300545c2a68f8d37b
domkalan <domkalan@gmail.com>
2024-10-27 23:52:35 -0700

    Logging error and warn

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,21 +1,19 @@
 export abstract class Logger {
-    public static readonly COLOR_RED = '\u001b[31m';
-    public static readonly COLOR_GREEN = '\u001b[32m';
-    public static readonly COLOR_YELLOW = '\u001b[33m';
-    public static readonly COLOR_BLUE = '\u001b[34m';
-    public static readonly COLOR_MAGENTA = '\u001b[35m';
-    public static readonly COLOR_CYAN = '\u001b[36m';
-    public static readonly COLOR_RESET = '\u001b[0m';
-
     public static Log(namespace : string, ...args : any) {
         const timeNow = new Date();
 
         console.log.apply(null, [`${timeNow.toLocaleTimeString()} [dokScreeps][${namespace}]`, ...args])
     }
 
+    public static Warn(namespace : string, ...args : any) {
+        const timeNow = new Date();
+
+        console.log.apply(null, [`${timeNow.toLocaleTimeString()} <font color="orange">[dokScreeps][${namespace}]`, ...args, '<font/>'])
+    }
+
     public static Error(namespace : string, ...args : any) {
         const timeNow = new Date();
 
-        console.error.apply(null, [`${this.COLOR_RED}${timeNow.toLocaleTimeString()} [dokScreeps:ERROR][${namespace}]`, ...args])
+        console.log.apply(null, [`${timeNow.toLocaleTimeString()} <font color="red">[dokScreeps][${namespace}]`, ...args, '<font/>'])
     }
 }
