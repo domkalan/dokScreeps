908805a59173708dc3daa87cde2ede0bbc2c818f
domkalan <domkalan@gmail.com>
2024-11-22 09:30:59 -0800

    Remove date from Logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,18 +2,18 @@ export abstract class Logger {
     public static Log(namespace : string, ...args : any) {
         const timeNow = new Date();
 
-        console.log.apply(null, [`${timeNow.toLocaleTimeString()} [dokScreeps][${namespace}]`, ...args])
+        console.log.apply(null, [`[dokScreeps][${namespace}]`, ...args])
     }
 
     public static Warn(namespace : string, ...args : any) {
         const timeNow = new Date();
 
-        console.log.apply(null, [`${timeNow.toLocaleTimeString()} <font color="orange">[dokScreeps][${namespace}]`, ...args, '<font/>'])
+        console.log.apply(null, [`<font color="orange">[dokScreeps][${namespace}]`, ...args, '<font/>'])
     }
 
     public static Error(namespace : string, ...args : any) {
         const timeNow = new Date();
 
-        console.log.apply(null, [`${timeNow.toLocaleTimeString()} <font color="red">[dokScreeps][${namespace}]`, ...args, '<font/>'])
+        console.log.apply(null, [`<font color="red">[dokScreeps][${namespace}]`, ...args, '<font/>'])
     }
 }
